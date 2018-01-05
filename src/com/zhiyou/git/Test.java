4536b877201a575187fbf5ab9e21c52f54ce1c48
lwj1987 <282948510@qq.com>
2018-01-05 22:03:11 +0800

    新创建了一个git练习项目

---
+++ src/com/zhiyou/git/Test.java
@@ -0,0 +1,18 @@
+package com.zhiyou.git;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午9:58:12
+*  
+*/
+public class Test {
+
+	
+	public static void main(String[] args) {
+		System.out.println("创建git");
+		
+	}
+}
+
