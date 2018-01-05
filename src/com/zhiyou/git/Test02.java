e5e63e5902c20f49dc2faeb0eb7a4f6710573e73
lwj1987 <282948510@qq.com>
2018-01-05 22:04:53 +0800

    新增test02

---
+++ src/com/zhiyou/git/Test02.java
@@ -0,0 +1,17 @@
+package com.zhiyou.git;
+
+/**  
+* @ClassName: Test02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午10:03:55
+*  
+*/
+public class Test02 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	}
+}
+
